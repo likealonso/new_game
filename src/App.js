6dfd92c9f3c1a454fdadd62ffbe0f5164a7a2533
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-04-07 22:21:04 -0700

    using css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -83,20 +83,20 @@ class App extends Component {
       }
     }
     
-    let classes = []
+    let assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
-      console.log(classes)
+      assignedClasses.push(classes.bold)
+    
     }
     
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>This is my new game</h1>
-          <p className={classes.join(' ')}>Or is it?</p>
+          <p className={assignedClasses.join(' ')}>Or is it?</p>
           <button 
             style={style}
             onClick={this.togglePersonHandler}>Toggle Persons
