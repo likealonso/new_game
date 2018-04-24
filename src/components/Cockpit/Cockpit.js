76cab778af91f817243491e35ee8fa087b353416
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-04-23 23:15:20 -0700

    splitting app into components

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import classes from './Cockpit.css'
+
+const cockpit = (props) => {
+
+    let assignedClasses = [];
+    let btnClass = ''
+    if (props.showPersons) {
+        btnClass = classes.red
+    }   
+
+    if (props.persons.length <= 2) {
+      assignedClasses.push(classes.red)
+    }
+    if (props.persons.length <= 1) {
+      assignedClasses.push(classes.bold)
+    }
+    return (
+        <div className={classes.Cockpit}>
+            <h1>This is my new game</h1>
+            <p className={assignedClasses.join(' ')}>Or is it?</p>
+            <button 
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons
+            </button>
+        </div>
+    )
+}
+
+export default cockpit
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
   state = {
@@ -53,39 +54,21 @@ class App extends Component {
 
   render() {
     let persons = null
-    let btnClass = ''
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          <Persons 
+      persons = <Persons 
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler}
-          />
-        </div> 
-      )
-      btnClass = classes.red
+            changed={this.nameChangedHandler} />
     }
-    
-    let assignedClasses = []
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red)
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold)
-    
-    }
-    
 
     return (
         <div className={classes.App}>
-          <h1>This is my new game</h1>
-          <p className={assignedClasses.join(' ')}>Or is it?</p>
-          <button 
-            className={btnClass}
-            onClick={this.togglePersonHandler}>Toggle Persons
-          </button>
+          <Cockpit 
+            showPersons={this.state.showPersons} 
+            persons={this.state.persons}  
+            clicked={this.togglePersonHandler}
+          />
           {persons}
         </div>
     );
