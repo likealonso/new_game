ae9c9e24d2721d65f70508d68725cd1b554f5630
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-04-24 13:33:28 -0700

    more lifecycles

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,6 +2,24 @@ import React from 'react'
 import Person from './Person/Person'
 
 class Persons extends React.Component {
+
+  componentWillReceiveProps(nextProps) {
+    console.log('UPDATE persons.js inside componentWillReceiveProps', nextProps)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('UPDATE persons.js inside shouldComponentUpdate', nextProps, 'whoa', nextState,'OK', this.props.persons)
+    return nextProps.persons !== this.props.persons
+  }
+
+  componentWilUpdate(nextProps, nextState) {
+    console.log('update persons.js inside componentWillUpdate', nextProps, nextState)
+  }
+
+  componentDidUpdate() {
+    console.log('update persons.js inside componentDidUpdate')
+  }
+
   render() {
     return this.props.persons.map((person, index) => {
       return <Person 
