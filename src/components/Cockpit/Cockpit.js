3892b389f604e7582c4ab0a24932cca6fb36aa8a
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-04-30 21:52:24 -0700

    add HOC

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import classes from './Cockpit.css'
+import Aux from '../../hoc/Aux'
 
 const cockpit = (props) => {
 
     let assignedClasses = [];
-    let btnClass = ''
+    let btnClass = classes.Button
     if (props.showPersons) {
-        btnClass = classes.red
+        btnClass = [classes.Button, classes.red].join(' ')
     }   
 
     if (props.persons.length <= 2) {
@@ -16,14 +17,14 @@ const cockpit = (props) => {
       assignedClasses.push(classes.bold)
     }
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>This is my new game</h1>
             <p className={assignedClasses.join(' ')}>Or is it?</p>
             <button 
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons
             </button>
-        </div>
+        </Aux>
     )
 }
 
